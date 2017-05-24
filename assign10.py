6a36ef2cfa4d885578f4f3f6b0bfbcf8ea40a329
jinpyojeon <jinpyo.jeon@trincoll.edu>
2017-05-24 12:58:06 -0400

    implemented monoid matrix mult

--- assign10.py
+++ assign10.py
@@ -1,19 +1,48 @@
 #!/usr/bin/env python
 
+from abc import ABCMeta, abstractmethod
+import numpy as np 
+
 Alice = {'name': 'Alice',
          'profession': 'Teacher',
          'age': 30 }
 
-class Matrix22:
+class Monoid():
+    __metaclass__ = ABCMeta
 
-    def __init__(self, x1, y1, x2, y2):
-        self.mat = [x1, y1, x2, y2] 
+    @abstractmethod
+    def e(): pass 
 
+    @abstractmethod
+    def op(x, y): pass
+
+class Matrix22():
+    def __init__(self, a, b, c, d):
+        self.mat = [a, b, c, d] 
+
+    def __str__(self):
+        return "[%d %d]\n[%d %d]" % (self.mat[0], self.mat[1], self.mat[2], self.mat[3]) 
+
+class Matrix22Multiplication(Monoid):
+    
+    def e(self): 
+        return Matrix22(1,0,0,1) # identity matrix
+
+    def op(self, x, y):
+        return Matrix22(x.mat[0] * y.mat[0] + x.mat[1] * y.mat[2], 
+                        x.mat[0] * y.mat[1] + x.mat[1] * y.mat[3],
+                        x.mat[2] * y.mat[0] + x.mat[3] * y.mat[2],
+                        x.mat[2] * y.mat[1] + x.mat[3] * y.mat[3])
+        
 
 def main():
 
     F = Matrix22(1,1,1,0)
+    mm = Matrix22Multiplication()
+    print mm.op( mm.op(F, F), F)
 
+    F_test = np.matrix('1 1; 1 0')
+    print (F_test * F_test * F_test)
 
 if __name__ == '__main__':
     main()
